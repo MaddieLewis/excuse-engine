5461e29f141119959364820f3a86e3a14573b451
Janie Amero <janie.amero@gmail.com>
2019-03-04 14:03:47 +0000

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :creative_excuses, only: [:show, :new, :create, :edit, :update]
+  resources :location_excuses, only: [:show, :new, :create]
+  resources :saved_excuses, except: [:index, :new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
