aa88593620c49d27d0fa08ee13bb9c7b64162cb0
Maddie Lewis <maddie@simplywellbeing.com>
2019-03-04 14:36:05 +0000

    controller for creative excuses finished

--- app/controllers/creative_excuses_controller.rb
+++ app/controllers/creative_excuses_controller.rb
@@ -1,6 +1,6 @@
 class CreativeExcusesController < ApplicationController
-    def show
-    @creative_excuse = CreativeExcuse.find(params[:id])
+  before_action :set_creative_excuse, only: %i[show edit update]
+  def show
   end
 
   def new
@@ -8,14 +8,33 @@ class CreativeExcusesController < ApplicationController
   end
 
   def create
-
+    @creative_excuse = CreativeExcuse.new(creative_excuse_params)
+    @creative_excuse.user = current_user
+    if @creative_excuse.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
   end
 
   def edit
-
   end
 
   def update
+    if @creative_excuse.update(creative_excuse_params)
+      redirect_to creative_excuse_path(@creative_excuse)
+    else
+      render :edit
+    end
+  end
 
+  private
+
+  def creative_excuse_params
+    params.require(:creative_excuse).permit(:title, :description, :photo)
+  end
+
+  def set_creative_excuse
+    @creative_excuse = CreativeExcuse.find(params[:id])
   end
 end
