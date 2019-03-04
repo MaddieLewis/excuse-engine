c2f9d7fb1bc8ade2e309d1696c730477d175c164
Mark Gould <m.gould16@gmail.com>
2019-03-04 16:03:29 +0000

    fixed seeding, need to see that .env has been added

--- app/models/creative_excuse.rb
+++ app/models/creative_excuse.rb
@@ -1,6 +1,6 @@
 class CreativeExcuse < ApplicationRecord
   belongs_to :user
   has_many :saved_excuses, as: :excuse
-  validates :title, :description, :photo
+  validates :title, :description, :photo, presence: true
   mount_uploader :photo, PhotoUploader
 end
