a37c8ed9b7c468c3f3d25a66dd217b0b2bde1267
Mark Gould <m.gould16@gmail.com>
2019-03-07 15:15:14 +0000

    added validation for category in creative excuse model

--- app/models/creative_excuse.rb
+++ app/models/creative_excuse.rb
@@ -1,6 +1,7 @@
 class CreativeExcuse < ApplicationRecord
   belongs_to :user
   has_many :saved_excuses, as: :excuse
-  validates :title, :description, :photo, presence: true
+  validates :title, :description, :category, presence: true
+  validates :category, inclusion { in: %w(malfunction pets health family funny other) }
   mount_uploader :photo, PhotoUploader
 end
