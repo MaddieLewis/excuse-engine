92b2849cd3b0e2644e99ae4314920c3049050773
alexandra campbell <killeraliens@outlook.com>
2019-03-08 14:35:08 +0000

    name params added to application controller for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :store_user_location!, if: :storable_location?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
     # Its important that the location is NOT stored if:
@@ -16,6 +17,14 @@ class ApplicationController < ActionController::Base
     def store_user_location!
       # :user is the scope we are authenticating
       store_location_for(:user, request.fullpath)
+    end
+
+    def configure_permitted_parameters
+      # For additional fields in app/views/devise/registrations/new.html.erb
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+      # For additional in app/views/devise/registrations/edit.html.erb
+      #devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :profile_picture, :description])
     end
 
     def default_url_options
