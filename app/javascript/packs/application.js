d1a9dc9c1630fcf98004f5cd7fbdb7b18817c99f
Janie Amero <janie.amero@gmail.com>
2019-03-11 12:45:33 +0000

    small changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,6 @@
 
 import "bootstrap";
-import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+// import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { autocomplete } from '../plugins/init_autocomplete';
 // import { initMapbox } from '../plugins/init_mapbox';
 
