2887685855b11b112a75118d0241c7739a30dcf8
Mark Gould <m.gould16@gmail.com>
2019-03-12 14:09:18 +0000

    added count logic to user level

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,33 +10,37 @@ class User < ApplicationRecord
   has_many :reported_excuses
   # after_create :send_welcome_email
 
-  def level
-    if self.saved_excuses.count <= 3
-      1
-    elsif self.saved_excuses.count > 3 && self.saved_excuses.count <= 5
-      2
-    elsif self.saved_excuses.count > 5 && self.saved_excuses.count <= 10
-      3
-    elsif self.saved_excuses.count > 10 && self.saved_excuses.count <= 15
-      4
-    elsif self.saved_excuses.count > 15
-      5
-    end
-  end
+def counter
+  return self.saved_excuses.count + self.creative_excuses.count + self.reported_excuses.count
+end
 
-  def icon
-    if level == 1
-      return "<i class='fas fa-battery-empty'></i>".html_safe
-    elsif level == 2
-      return "<i class='fas fa-battery-quarter'></i>".html_safe
-    elsif level == 3
-      return "<i class='fas fa-battery-half'></i>".html_safe
-    elsif level == 4
-      return "<i class='fas fa-battery-three-quarters'></i>".html_safe
-    elsif level <= 5
-      return "<i class='fas fa-battery-full'></i>".html_safe
-    end
+def level
+  if counter <= 3
+    1
+  elsif counter > 3 && counter <= 5
+    2
+  elsif counter > 5 && counter <= 10
+    3
+  elsif counter > 10 && counter <= 15
+    4
+  elsif counter > 15
+    5
   end
+end
+
+def icon
+  if level == 1
+    return "<i class='fas fa-battery-empty'></i>".html_safe
+  elsif level == 2
+    return "<i class='fas fa-battery-quarter'></i>".html_safe
+  elsif level == 3
+    return "<i class='fas fa-battery-half'></i>".html_safe
+  elsif level == 4
+    return "<i class='fas fa-battery-three-quarters'></i>".html_safe
+  elsif level > 5
+    return "<i class='fas fa-battery-full'></i>".html_safe
+  end
+end
 
   private
 
