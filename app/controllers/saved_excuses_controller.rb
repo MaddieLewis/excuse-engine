2a1c7a6c8e53c4a56f83f44b8fe3a90642dddde0
Maddie Lewis <maddie@simplywellbeing.com>
2019-03-13 18:13:10 +0000

    hearts work everywhere and show on users

--- app/controllers/saved_excuses_controller.rb
+++ app/controllers/saved_excuses_controller.rb
@@ -10,18 +10,35 @@ class SavedExcusesController < ApplicationController
   end
 
   def create
-    cu = CreativeExcuse.find(params[:creative_excuse_id].to_i)
-    @se = SavedExcuse.new(excuse: cu, user: current_user)
-    if @se.save
-      # redirect_to creative_excuse_path(cu)
-      respond_to do |format|
-        format.html { redirect_to creative_excuse_path(cu) , notice: 'Your excuse was saved.' }
-        format.js
+    if params[:creative_excuse_id]
+      cu = CreativeExcuse.find(params[:creative_excuse_id].to_i)
+      @se = SavedExcuse.new(excuse: cu, user: current_user)
+      if @se.save
+        # redirect_to creative_excuse_path(cu)
+        respond_to do |format|
+          format.html { redirect_to creative_excuse_path(cu) , notice: 'Your excuse was saved.' }
+          format.js
+        end
+      else
+        respond_to do |format|
+          format.html { render template: 'creative_excuses/show' }
+          format.js
+        end
       end
-    else
-      respond_to do |format|
-        format.html { render template: 'creative_excuses/show' }
-        format.js
+    elsif params[:location_excuse_id]
+      le = LocationExcuse.find(params[:location_excuse_id])
+      @se = SavedExcuse.new(excuse: le, user: current_user)
+      if @se.save
+        # redirect_to creative_excuse_path(cu)
+        respond_to do |format|
+          format.html { redirect_to location_excuse_path(le) , notice: 'Your excuse was saved.' }
+          format.js
+        end
+      else
+        respond_to do |format|
+          format.html { render template: 'location_excuses/show' }
+          format.js
+        end
       end
     end
   end
@@ -38,7 +55,7 @@ class SavedExcusesController < ApplicationController
   private
 
   def saved_excuse_params
-    params.require(:saved_excuse).permit(:message, :rating)
+    params.require(:saved_excuse).permit(:message, :rating, :type)
   end
 
   def set_saved_excuse
