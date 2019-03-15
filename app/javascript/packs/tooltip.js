8000f776adb5d92580f84cd0d745e02111547b35
Janie Amero <janie.amero@gmail.com>
2019-03-15 15:26:13 +0000

    static loader added for ios

--- app/javascript/packs/tooltip.js
+++ app/javascript/packs/tooltip.js
@@ -1,12 +1,15 @@
 const tooltip = () => {
-  document.getElementById('copy-button').addEventListener('click', (event) => {
-    const newValue = "Copied"
-    $('[data-toggle="tooltip"]').tooltip()
-            .attr('data-original-title', newValue)
-            .tooltip('update')
-            .tooltip('hide')
-            .tooltip('show');
-  })
+  const copy = document.getElementById('copy-button');
+  if (copy) {
+    copy.addEventListener('click', (event) => {
+      const newValue = "Copied"
+      $('[data-toggle="tooltip"]').tooltip()
+              .attr('data-original-title', newValue)
+              .tooltip('update')
+              .tooltip('hide')
+              .tooltip('show');
+    })
+  }
 }
 export { tooltip };
 
